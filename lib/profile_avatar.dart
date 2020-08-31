08095d9b23cdeed12cc0b5417a898ba2f0f11c0a
Renzo Olivares <renzo.olivares1@gmail.com>
2020-08-31 13:31:22 -0700

    Bring in reply from gallery

---
+++ lib/profile_avatar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ProfileAvatar extends StatelessWidget {
+  const ProfileAvatar({
+    @required this.avatar,
+    this.radius = 20,
+  }) : assert(avatar != null);
+
+  final String avatar;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: CircleAvatar(
+        radius: radius,
+        backgroundColor: Theme.of(context).cardColor,
+        child: ClipOval(
+          child: Image.asset(
+            avatar,
+            height: 42,
+            width: 42,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
