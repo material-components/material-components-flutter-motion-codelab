605479075b295e687c76e4c86ac4762809e82a12
Renzo Olivares <renzo.olivares1@gmail.com>
2020-09-13 21:34:24 -0700

    Remove open container between fab and compose page

--- lib/home.dart
+++ lib/home.dart
@@ -750,42 +750,40 @@ class _ReplyFabState extends State<_ReplyFab>
         );
         final tooltip = onMailView ? 'Reply' : 'Compose';
 
-        return OpenContainer(
-          openBuilder: (context, closedContainer) {
-            return const ComposePage();
-          },
-          openColor: theme.cardColor,
-          onClosed: (success) {
-            Provider.of<EmailStore>(
-              context,
-              listen: false,
-            ).onCompose = false;
-          },
-          closedShape: circleFabBorder,
-          closedColor: theme.colorScheme.secondary,
-          closedElevation: 6,
-          closedBuilder: (context, openContainer) {
-            return Tooltip(
-              message: tooltip,
-              child: InkWell(
-                customBorder: circleFabBorder,
-                onTap: () {
-                  Provider.of<EmailStore>(
-                    context,
-                    listen: false,
-                  ).onCompose = true;
-                  openContainer();
-                },
-                child: SizedBox(
-                  height: _mobileFabDimension,
-                  width: _mobileFabDimension,
-                  child: Center(
-                    child: fabSwitcher,
+        // TODO: Add Container Transform from FAB to compose email page (Motion)
+        return Material(
+          color: theme.colorScheme.secondary,
+          child: Tooltip(
+            message: tooltip,
+            child: InkWell(
+              customBorder: circleFabBorder,
+              onTap: () {
+                Provider.of<EmailStore>(
+                  context,
+                  listen: false,
+                ).onCompose = true;
+
+                Navigator.of(context).push(
+                  PageRouteBuilder(
+                    pageBuilder: (
+                      BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnimation,
+                    ) {
+                      return const ComposePage();
+                    },
                   ),
+                );
+              },
+              child: SizedBox(
+                height: _mobileFabDimension,
+                width: _mobileFabDimension,
+                child: Center(
+                  child: fabSwitcher,
                 ),
               ),
-            );
-          },
+            ),
+          ),
         );
       },
     );
